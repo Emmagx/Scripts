c1514bca1e185fe96b79ee84f2421c9ff96f30aa
Emmagx <125303962+Emmagx@users.noreply.github.com>
2024-04-28 19:24:02 -0600

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -7,12 +7,12 @@ get_timestamp() {
 
 # Función para realizar el speedtest y guardar los resultados en el archivo CSV
 run_speedtest() {
-    speedtest-cli --secure --csv | awk -F ',' -v OFS=',' '{print $1, $2, $7/8/1024/1024, $8/8/1024/1024}' >> /home/bitmecnoc/Registros/speedtest.csv
+    speedtest-cli --secure --csv | awk -F ',' -v OFS=',' '{print $1, $2, $7/8/1024/1024, $8/8/1024/1024}' >> /YOUR/FAVORITE/ROUTE/speedtest.csv
 }
 
 # Encabezado del archivo CSV si no existe
-if [ ! -f /home/bitmecnoc/Registros/speedtest.csv ]; then
-    echo "Date,Time,Download (MB/s),Upload (MB/s) \n" > /home/bitmecnoc/Registros/speedtest.csv
+if [ ! -f /YOUR/FAVORITE/ROUTE/speedtest.csv ]; then
+    echo "Date,Time,Download (MB/s),Upload (MB/s) \n" > /YOUR/FAVORITE/ROUTE/speedtest.csv
 fi
 
 run_speedtest
